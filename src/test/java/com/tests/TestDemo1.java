a24703025293af9a97ae91870696fe9cfaea780c
AjayRaj7059 <rajeshsable4u@gmail.com>
2021-07-04 23:40:11 -0700

    Added a new Test case

--- src/test/java/com/tests/TestDemo1.java
+++ src/test/java/com/tests/TestDemo1.java
@@ -3,9 +3,17 @@ package com.tests;
 import org.testng.annotations.Test;
 
 public class TestDemo1 {
+	
 	@Test
 	public void test(){
 		System.out.println("Hellooo.......!!!");
 	}
+	
+
+	@Test
+	public void tes2t(){
+		System.out.println("Hellooo.......!!!");
+	}
+
 
 }
